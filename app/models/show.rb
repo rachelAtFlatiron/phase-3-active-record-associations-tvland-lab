b63a4a00afe3afc805b99ba7879844a7c81df36e
Rachel Katz <rachel.katz@flatironschool.com>
2022-09-07 12:54:33 -0400

    commit tree 8785537f54d011cf804c4354bcf8050771e5517e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :actors, through: :characters 
+  belongs_to :network
+
+  def actors_list 
+    names = []
+    Show.first.characters.each{
+        |character|
+        names << "#{character.actor.first_name} #{character.actor.last_name}"
+    }
+    names
+  end 
 end
